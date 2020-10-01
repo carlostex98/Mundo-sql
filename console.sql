2b6e9174173a3ce6ca583173902893151939aabf
Carlos Tenes <cctenes@gmail.com>
2020-09-30 18:49:31 -0600

    Update

--- console.sql
+++ console.sql
@@ -3,6 +3,10 @@ use mundo;
 
 drop table datos_entrada;
 truncate table datos_entrada;
+
+drop database mundo;
+create database mundo;
+
 create table datos_entrada(
     nombre_compania varchar(220),
     contacto_compania varchar(220),
@@ -34,7 +38,38 @@ load data infile '/var/lib/mysql-files/DataCenterData.csv' into table datos_entr
     telefono, @date_time_variable,direccion, ciudad, codigo_postal, region, producto,categoria_producto, cantidad, precio_unitario)
     SET fecha_registro = STR_TO_DATE(@date_time_variable, '%d/%m/%Y');
 
+select count(*) from datos_entrada;
 
+
+
+select * from datos_entrada limit 10;
+
+select distinct producto from datos_entrada;
+select distinct nombre from datos_entrada where tipo='C';
+select producto from datos_entrada;
+select distinct tipo from datos_entrada;
+
+select max(precio_unitario), min(precio_unitario) from datos_entrada;
+
+select nombre_compania, contacto_compania, correo_compania, telefono_compania from datos_entrada
+group by nombre_compania;
+
+select distinct nombre_compania from datos_entrada;
+select nombre_compania,  nombre from datos_entrada where nombre_compania='Dolor Sit Amet Corp.' and tipo='C';
+select id_compania from companias where nombre_compania = 'Dolor Sit Amet Corp.' ;
+
+
+
+select distinct nombre from datos_entrada where tipo='C';
+select nombre, correo, telefono, fecha_registro, direccion, codigo_postal from datos_entrada where nombre = 'Shea Jensen' limit 1;
+
+select distinct producto from datos_entrada;
+select producto, categoria_producto from datos_entrada where producto='Cauliflower';
+
+
+
+
+select * from companias;
 
 
 create table categorias(
@@ -42,13 +77,18 @@ create table categorias(
     nombre_categoria varchar(220)
 )engine = InnoDB charset =utf8mb4 collate utf8mb4_general_ci;
 
+drop table companias;
 
 create table companias (
     id_compania int auto_increment primary key,
+    nombre_compania varchar(220),
     contacto varchar(220),
     correo varchar(220),
     telefono varchar(220)
 );
+
+
+describe companias;
 
 create table clientes(
     id_cliente int auto_increment primary key,
@@ -92,6 +132,8 @@ create table compras(
     cantidad_compra int
 );
 
+
+
 create table cliente_compra_producto(
     id_compra_fk int,
     id_cliente_fk int,
@@ -126,7 +168,7 @@ create table proveedor_region(
     foreign key (id_proveedor_fk) references proveedores(id_proveedor)
 );
 
-
+-- yep --
 create table cliente_ciudad(
     id_ciudad_fk int,
     id_cliente_fk int,
@@ -134,6 +176,7 @@ create table cliente_ciudad(
     foreign key (id_cliente_fk) references clientes(id_cliente)
 );
 
+-- yep --
 create table proveedor_ciudad(
     id_ciudad_fk int,
     id_proveedor_fk int,
@@ -141,7 +184,7 @@ create table proveedor_ciudad(
     foreign key (id_proveedor_fk) references proveedores(id_proveedor)
 );
 
-
+-- yep --
 create table producto_categoria(
     id_producto_fk int,
     id_categoria_fk int,
@@ -149,16 +192,69 @@ create table producto_categoria(
     foreign key (id_producto_fk) references productos(id_producto)
 );
 
+-- yep --
 create table cliente_compania(
-    id_ciudad_fk int,
+    id_cliente_fk int,
     id_compania_fk int,
-    foreign key (id_ciudad_fk) references ciudades(id_ciudad),
+    foreign key (id_cliente_fk) references clientes(id_cliente),
     foreign key (id_compania_fk) references companias(id_compania)
 );
 
+-- yep --
 create table proveedor_compania(
-    id_ciudad_fk int,
+    id_proveedor_fk int,
     id_compania_fk int,
-    foreign key (id_ciudad_fk) references ciudades(id_ciudad),
+    foreign key (id_proveedor_fk) references proveedores(id_proveedor),
     foreign key (id_compania_fk) references companias(id_compania)
 );
+
+
+
+
+delete from categorias where id_categoria>-1;
+delete from ciudades where id_ciudad>-1;
+delete from regiones where id_region>-1;
+delete from companias where id_compania>-1;
+delete from clientes where id_cliente>-1;
+delete from proveedores where id_proveedor>-1;
+delete from productos where id_producto>-1;
+TRUNCATE TABLE mundo.cliente_ciudad;
+TRUNCATE TABLE mundo.cliente_compania;
+TRUNCATE TABLE mundo.cliente_compra_producto;
+TRUNCATE TABLE mundo.cliente_region;
+TRUNCATE TABLE mundo.datos_entrada;
+TRUNCATE TABLE mundo.producto_categoria;
+TRUNCATE TABLE mundo.proveedor_ciudad;
+TRUNCATE TABLE mundo.proveedor_compania;
+TRUNCATE TABLE mundo.proveedor_compra_producto;
+TRUNCATE TABLE mundo.proveedor_region;
+
+
+
+select * from proveedores;
+select * from categorias;
+
+
+select * from productos;
+select * from ciudades ;
+select * from clientes;
+select * from companias;
+select * from regiones;
+
+select * from cliente_compania;
+select * from proveedor_compania;
+select * from producto_categoria;
+select * from proveedor_ciudad;
+select  * from cliente_ciudad;
+select * from proveedor_region;
+select * from cliente_region;
+
+
+
+
+select id_cliente, nombre from clientes;
+select id_compania, nombre_compania from companias;
+
+
+SELECT Concat('TRUNCATE TABLE ',table_schema,'.',TABLE_NAME, ';')
+FROM INFORMATION_SCHEMA.TABLES where  table_schema in ('mundo');
--- src/helper/fullDatabase.js
+++ src/helper/fullDatabase.js
@@ -1,27 +1,280 @@
 const pool = require('../database');
 
 async function filModel() {
+
+    //ciudades();
+    //regiones();
+    //categorias();
+
+    //the group join
+
+    //companias();
+    //clientes();
+    //proveedores();
+    //productos();
+
+    //cliente_compania();
+    //proveedor_compania();
+    //producto_categoria();
+    //proveedor_ciudad();
+    //cliente_ciudad();
+    //proveedor_region();
+    cliente_region();
+
+
+}
+
+
+async function ciudades() {
+    const cd = await pool.query('select distinct ciudad from datos_entrada');
+    for (let i = 0; i < cd.length; i++) {
+        let nombre = cd[i].ciudad;
+        await pool.query("insert into ciudades(nombre_ciudad) values(\'" + nombre + "\')");
+    }
+}
+
+async function regiones() {
+    const r = await pool.query('select distinct region from datos_entrada');
+    for (let i = 0; i < r.length; i++) {
+        let nombre = r[i].region;
+        await pool.query("insert into regiones(nombre_region) values(\'" + nombre + "\')");
+    }
+}
+
+
+async function categorias() {
     const c = await pool.query('select distinct categoria_producto from datos_entrada');
     for (let i = 0; i < c.length; i++) {
         let nombre = c[i].categoria_producto;
         //insert categoria
-        await pool.query("insert into categorias(nombre_categoria) values(\'"+nombre+"\')");
+        await pool.query("insert into categorias(nombre_categoria) values(\'" + nombre + "\')");
     }
-
-    const r = await pool.query('select distinct region from datos_entrada');
-    for (let i = 0; i < r.length; i++) {
-        let nombre = r[i].region;
-        await pool.query("insert into regiones(nombre_region) values(\'"+nombre+"\')");
-    }
-
-    const cd = await pool.query('select distinct ciudad from datos_entrada');
-    for (let i = 0; i < cd.length; i++) {
-        let nombre = cd[i].region;
-        await pool.query("insert into ciudades(nombre_ciudad) values(\'"+nombre+"\')");
-    }
-
-
-    
 }
+
+
+async function companias() {
+    const comp = await pool.query('select distinct nombre_compania from datos_entrada');
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select nombre_compania, contacto_compania, correo_compania, telefono_compania from datos_entrada " +
+            "where nombre_compania = '" + comp[i].nombre_compania + "' limit 1");
+        //insert into companias
+        await pool.query("insert into companias(nombre_compania, contacto, correo, telefono) values(" +
+            "\'" + k[0].nombre_compania + "\'," +
+            "\'" + k[0].contacto_compania + "\'," +
+            "\'" + k[0].correo_compania + "\'," +
+            "\'" + k[0].telefono_compania + "\'" +
+            ")");
+    }
+}
+
+async function clientes() {
+    const nomb = await pool.query("select distinct nombre from datos_entrada where tipo='C'");
+    for (let i = 0; i < nomb.length; i++) {
+        let t = "select nombre, correo, telefono, fecha_registro, direccion, codigo_postal " +
+            "from datos_entrada where nombre = '" + nomb[i].nombre + "' limit 1";
+        let k = await pool.query(t);
+        //insert into companias
+        await pool.query("insert into clientes(nombre, correo, telefono, fecha_reg, direccion, codigo_postal) values(" +
+            "'" + k[0].nombre + "'," +
+            "'" + k[0].correo + "'," +
+            "'" + k[0].telefono + "'," +
+            "'" + fix_date(k[0].fecha_registro) + "'," +
+            "'" + k[0].direccion + "'," +
+            "'" + k[0].codigo_postal + "'" +
+            ")");
+    }
+}
+
+async function proveedores() {
+    const nomb = await pool.query("select distinct nombre from datos_entrada where tipo='P'");
+    for (let i = 0; i < nomb.length; i++) {
+        let t = "select nombre, correo, telefono, fecha_registro, direccion, codigo_postal " +
+            "from datos_entrada where nombre = '" + nomb[i].nombre + "' limit 1";
+        let k = await pool.query(t);
+        //insert into companias
+        await pool.query("insert into proveedores(nombre, correo, telefono, fecha_reg, direccion, codigo_postal) values(" +
+            "'" + k[0].nombre + "'," +
+            "'" + k[0].correo + "'," +
+            "'" + k[0].telefono + "'," +
+            "'" + fix_date(k[0].fecha_registro) + "'," +
+            "'" + k[0].direccion + "'," +
+            "'" + k[0].codigo_postal + "'" +
+            ")");
+    }
+}
+
+async function productos() {
+    const nomb = await pool.query("select distinct producto from datos_entrada");
+    for (let i = 0; i < nomb.length; i++) {
+        let t = "select producto, precio_unitario " +
+            "from datos_entrada where producto = '" + nomb[i].producto + "' limit 1";
+        let k = await pool.query(t);
+        //console.log(k);
+        //insert into companias
+        await pool.query("insert into productos(nombre_producto, precio_unitario) values(" +
+            "'" + k[0].producto + "'," +
+            "'" + k[0].precio_unitario + "'" +
+            ")");
+
+    }
+}
+
+
+async function cliente_compania() {
+    const comp = await pool.query('select distinct nombre_compania from datos_entrada');
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select nombre_compania,  nombre from datos_entrada " +
+            "where nombre_compania='" + comp[i].nombre_compania + "' and tipo='C'");
+        for (let j = 0; j < k.length; j++) {
+            let idv = await pool.query("select id_compania from companias where nombre_compania = '" + k[j].nombre_compania + "' ;");
+            let idx = await pool.query("select id_cliente from clientes where nombre = '" + k[j].nombre + "' ;");
+            let h = await pool.query("select count(*) as x from cliente_compania where " +
+                "id_cliente_fk = " + idx[0].id_cliente + " and " +
+                "id_compania_fk = " + idv[0].id_compania);
+            if (h[0].x == 0) {
+                await pool.query("insert into cliente_compania values(" + idx[0].id_cliente + ", " + idv[0].id_compania + ")");
+            }
+        }
+
+    }
+}
+
+
+async function proveedor_compania() {
+    const comp = await pool.query('select distinct nombre_compania from datos_entrada');
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select nombre_compania,  nombre from datos_entrada " +
+            "where nombre_compania='" + comp[i].nombre_compania + "' and tipo='P'");
+        for (let j = 0; j < k.length; j++) {
+            let idv = await pool.query("select id_compania from companias where nombre_compania = '" + k[j].nombre_compania + "' ;");
+            let idx = await pool.query("select id_proveedor from proveedores where nombre = '" + k[j].nombre + "' ;");
+            let h = await pool.query("select count(*) as x from proveedor_compania where " +
+                "id_proveedor_fk = " + idx[0].id_proveedor + " and " +
+                "id_compania_fk = " + idv[0].id_compania);
+            if (h[0].x == 0) {
+                await pool.query("insert into proveedor_compania values(" + idx[0].id_proveedor + ", " + idv[0].id_compania + ")");
+            }
+        }
+
+    }
+}
+
+
+async function producto_categoria() {
+    const comp = await pool.query('select distinct producto from datos_entrada');
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select producto, categoria_producto from datos_entrada " +
+            "where producto='" + comp[i].producto + "'");
+        for (let j = 0; j < k.length; j++) {
+            let idv = await pool.query("select id_producto from productos where nombre_producto = '" + k[j].producto + "' ;");
+            let idx = await pool.query("select id_categoria from categorias where nombre_categoria = '" + k[j].categoria_producto + "' ;");
+            let h = await pool.query("select count(*) as x from producto_categoria where " +
+                "id_categoria_fk = " + idx[0].id_categoria + " and " +
+                "id_producto_fk = " + idv[0].id_producto);
+            if (h[0].x == 0) {
+                await pool.query("insert into producto_categoria values(" + idv[0].id_producto + ", " + idx[0].id_categoria + ")");
+            }
+        }
+
+    }
+}
+
+async function proveedor_ciudad() {
+    const comp = await pool.query("select distinct nombre from datos_entrada where tipo='P'");
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select nombre, ciudad from datos_entrada " +
+            "where nombre='" + comp[i].nombre + "' and tipo='P'");
+        for (let j = 0; j < k.length; j++) {
+            let idx = await pool.query("select id_proveedor from proveedores where nombre = '" + k[j].nombre + "' ;");
+            let idv = await pool.query("select id_ciudad from ciudades where nombre_ciudad = '" + k[j].ciudad + "' ;");
+            let h = await pool.query("select count(*) as x from proveedor_ciudad where " +
+                "id_proveedor_fk = " + idx[0].id_proveedor + " and " +
+                "id_ciudad_fk = " + idv[0].id_ciudad );
+            if (h[0].x == 0) {
+                await pool.query("insert into proveedor_ciudad values(" + idv[0].id_ciudad + ", " + idx[0].id_proveedor + ")");
+            }
+        }
+
+    }
+}
+
+async function cliente_ciudad() {
+    const comp = await pool.query("select distinct nombre from datos_entrada where tipo='C'");
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select nombre, ciudad from datos_entrada " +
+            "where nombre='" + comp[i].nombre + "' and tipo='C'");
+        for (let j = 0; j < k.length; j++) {
+            let idx = await pool.query("select id_cliente from clientes where nombre = '" + k[j].nombre + "' ;");
+            let idv = await pool.query("select id_ciudad from ciudades where nombre_ciudad = '" + k[j].ciudad + "' ;");
+            let h = await pool.query("select count(*) as x from cliente_ciudad where " +
+                "id_cliente_fk = " + idx[0].id_cliente+ " and " +
+                "id_ciudad_fk = " + idv[0].id_ciudad );
+            if (h[0].x == 0) {
+                await pool.query("insert into cliente_ciudad values(" + idv[0].id_ciudad + ", " + idx[0].id_cliente + ")");
+            }
+        }
+
+    }
+}
+
+async function proveedor_region() {
+    const comp = await pool.query("select distinct nombre from datos_entrada where tipo='P'");
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select nombre, region from datos_entrada " +
+            "where nombre='" + comp[i].nombre + "' and tipo='P'");
+        for (let j = 0; j < k.length; j++) {
+            let idx = await pool.query("select id_proveedor from proveedores where nombre = '" + k[j].nombre + "' ;");
+            let idv = await pool.query("select id_region from regiones where nombre_region = '" + k[j].region + "' ;");
+            let h = await pool.query("select count(*) as x from proveedor_region where " +
+                "id_proveedor_fk = " + idx[0].id_proveedor + " and " +
+                "id_region_fk = " + idv[0].id_region );
+            if (h[0].x == 0) {
+                await pool.query("insert into proveedor_region values(" + idv[0].id_region + ", " + idx[0].id_proveedor + ")");
+            }
+        }
+
+    }
+}
+
+async function cliente_region() {
+    const comp = await pool.query("select distinct nombre from datos_entrada where tipo='C'");
+    for (let i = 0; i < comp.length; i++) {
+
+        let k = await pool.query("select nombre, region from datos_entrada " +
+            "where nombre='" + comp[i].nombre + "' and tipo='C'");
+        for (let j = 0; j < k.length; j++) {
+            let idx = await pool.query("select id_cliente from clientes where nombre = '" + k[j].nombre + "' ;");
+            let idv = await pool.query("select id_region from regiones where nombre_region = '" + k[j].region + "' ;");
+            let h = await pool.query("select count(*) as x from cliente_region where " +
+                "id_cliente_fk = " + idx[0].id_cliente+ " and " +
+                "id_region_fk = " + idv[0].id_region );
+            if (h[0].x == 0) {
+                await pool.query("insert into cliente_region values(" + idv[0].id_region + ", " + idx[0].id_cliente + ")");
+            }
+        }
+
+    }
+}
+
+
+
+
+
+function fix_date(p) {
+    var today = new Date(p);
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+}
+
 
 module.exports.filModel = filModel;
